8e0a525a69801ec88bbef122d78f4043cf5dc5c7
Charles Dai <charles.r.dai@gmail.com>
2026-06-13 21:33:46 -0400

    Refactored to eliminate redundant code

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,68 @@
-import { MarkdownRenderer, Plugin } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { } from './settings';
 
 const CLOZE_REGEX = /\{(?:\d+:)?([^:}]+)(?:::[^}]*?)?\}/g;
-const BLOCK_TAGS_INCLUDED = ["p", "li", "table"];
-const BLOCK_TAGS_EXCLUDED = ["code", "blockquote"];
 
 export default class ClozePlugin extends Plugin {
 	async onload() {
 		console.clear()
 		this.registerMarkdownPostProcessor((el: HTMLElement) => {
-			this.labelEmptyFlashcardLines(el);
-			this.processClozes(el);
+			this.labelEmptyCardLines(el);
+			this.renderCardClozes(el);
 		});
 	}
 
+	/* Check if a node is either empty or only contains a linebreak */
+	private isEffectivelyEmpty(node: Node): boolean {
+		if (node.nodeType === Node.TEXT_NODE) {
+			return node.textContent?.trim() === '';
+		}
+		if (node.nodeType === Node.ELEMENT_NODE) {
+			return node.nodeName === 'BR';
+		}
+		return false;
+	}
+	/*
+	Label paragraphs within callout blocks as empty, for css to pick up on.
+	Essentially to hide property data in the beginning of flashcards.
+	*/
+	private labelEmptyCardLines(el: HTMLElement) {
+		const paragraphs = el.querySelectorAll(
+			'.callout[data-callout="card"] .callout-content p'
+		);
 
-	private labelEmptyFlashcardLines(el: HTMLElement) {
-		el.querySelectorAll('.callout[data-callout="card"] .callout-content p')
-			.forEach(p => {
-				console.log(p.nodeName)
-				const effectivelyEmpty = [...p.childNodes].every(node => {
-					if (!node) return true;
-					if (node.nodeType === Node.TEXT_NODE) {
-						return node.textContent?.trim() === '';
-					}
-					if (node.nodeType === Node.ELEMENT_NODE) {
-						return node.nodeName === 'BR';
-					}
-					return false;
-				});
+		paragraphs.forEach(p => {
+			const effectivelyEmpty =
+				p.childNodes.length === 0 ||
+				[...p.childNodes].every(node => this.isEffectivelyEmpty(node));
 
-				if (effectivelyEmpty) {
-					p.setAttr('data-card-empty', true);
-				}
-			});
+			if (effectivelyEmpty) {
+				p.setAttr('data-card-empty', 'true');
+			}
+		});
 	}
 
-	private isValidTextNode(node: Node) {
-		return (node.parentElement?.closest(BLOCK_TAGS_INCLUDED.join(", ")) !== null)
-			&& (node.parentElement?.closest(BLOCK_TAGS_EXCLUDED.join(", ")) === null)
-			&& !!node.textContent?.trim();
+	/*
+	Find and render all clozes in flashcards in given block.
+	*/
+	private renderCardClozes(el: HTMLElement) {
+		const cards = el.querySelectorAll('.callout[data-callout="card"]')
+		cards.forEach(card => this.renderClozeSpans(card));
 	}
 
-	private processClozes(el: HTMLElement) {
+	private readonly clozeRe = new RegExp(CLOZE_REGEX.source, CLOZE_REGEX.flags);
+
+	/*
+	Given a flashcard, replace all clozes in the form {1:foo::bar} with 
+	span containing the answer text. 
+	*/
+	private renderClozeSpans(el: Element) {
 		const walker = document.createTreeWalker(
 			el,
 			NodeFilter.SHOW_TEXT,
 			{
 				acceptNode: (node: Node) => {
-					return this.isValidTextNode(node)
+					return !!node.textContent?.trim()
 						? NodeFilter.FILTER_ACCEPT
 						: NodeFilter.FILTER_SKIP;
 				}
@@ -62,28 +76,30 @@ export default class ClozePlugin extends Plugin {
 			current = walker.nextNode() as Text | null;
 		}
 
-		const re = new RegExp(CLOZE_REGEX.source, CLOZE_REGEX.flags)
 		blocks.forEach((textblock) => {
-			if (!textblock) return;
+			const text = textblock?.textContent;
 
-			const text = textblock?.textContent ?? "";
+			this.clozeRe.lastIndex = 0;
+			if (!this.clozeRe.test(text)) return;
+			this.clozeRe.lastIndex = 0;
 
 			// build new text to put into the block
-			let hasCloze = false;
 			const fragment = document.createDocumentFragment();
 			let lastIndex = 0;
-			for (const match of text.matchAll(re)) {
-				hasCloze = true;
-				fragment.append(text.slice(lastIndex, match.index));
-				fragment.createSpan({ text: match[1] });
+			for (const match of text.matchAll(this.clozeRe)) {
+				const slice = text.slice(lastIndex, match.index)
+				if (slice) fragment.append(slice);
+				fragment.createSpan({ 
+					text: match[1],
+					cls: 'cloze',
+				});
 				lastIndex = (match.index ?? 0) + match[0].length;
 			}
-			if (!hasCloze) return;
-
 			// add tail end of text
-			fragment.append(text.slice(lastIndex));
+			const posttext = text.slice(lastIndex);
+			if (posttext) fragment.append(posttext);
+
 			textblock.replaceWith(fragment);
 		});
 	}
-
 }
